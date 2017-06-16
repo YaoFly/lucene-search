6afbe9a71fa5c727bf7f0c010e2f3d55a85c0e16
yaofly <2465939235@qq.com>
2017-06-16 17:39:18 +0800

    ...

--- core/src/main/java/control/HitEntry.java
+++ core/src/main/java/control/HitEntry.java
@@ -6,19 +6,13 @@ public class HitEntry{
     private String tags="";
     private String keyword="";
 
-    public void setScore(double sc){score=sc;}
-    public double getScore(){return score;}
 
     public void setId(String d){id=d;}
     public String getId(){return id;}
 
-    public void setTags(String t){tags=t;}
-    public String getTags(){return tags;}
-
     public String getKeyword() {
         return keyword;
     }
-
     public void setKeyword(String keyword) {
         this.keyword = keyword;
     }
